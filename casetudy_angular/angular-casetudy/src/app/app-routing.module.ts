bc0aa0f4c33d72afdbb42f0315d4cf4a9d1de020
Your Name <you@example.com>
2023-02-07 16:02:52 +0700

    update

--- casetudy_angular/angular-casetudy/src/app/app-routing.module.ts
+++ casetudy_angular/angular-casetudy/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListFacilityComponent} from "./component/facility/list-facility/list-facility.component";
+import {BodyComponent} from "./component/body/body.component";
+import {CreateFacilityComponent} from "./component/facility/create-facility/create-facility.component";
+import {EditFacilityComponent} from "./component/facility/edit-facility/edit-facility.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',pathMatch: 'full',redirectTo: 'home'},
+  {path: '',component: BodyComponent},
+  {path: 'home',component: BodyComponent},
+  {path: 'facility',component: ListFacilityComponent},
+  {path: 'createFacility',component: CreateFacilityComponent},
+  {path: 'editFacility',component: EditFacilityComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- casetudy_angular/angular-casetudy/src/app/app.module.ts
+++ casetudy_angular/angular-casetudy/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { BodyComponent } from './component/body/body.component';
+import { CreateFacilityComponent } from './component/facility/create-facility/create-facility.component';
+import {ListFacilityComponent} from "./component/facility/list-facility/list-facility.component";
+import { EditFacilityComponent } from './component/facility/edit-facility/edit-facility.component';
+
 
 
 @NgModule({
@@ -13,7 +17,10 @@ import { BodyComponent } from './body/body.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    CreateFacilityComponent,
+    ListFacilityComponent,
+    EditFacilityComponent
   ],
   imports: [
     BrowserModule,
